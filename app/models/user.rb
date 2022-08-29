38469f7ffff07c657b17b9e4fd3dd953a0615c84
Reinaldo <r.guerra.villagra@gmail.com>
2022-08-29 01:37:25 -0400

    correcciones

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
     validates :email, presence: true, uniqueness: true
     validates :name, :password, presence: true
   
-    has_many :figures
+    has_many :transactions
 end
